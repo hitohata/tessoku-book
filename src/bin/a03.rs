bec3eab1f43ad55dbbae777814918df8b332a7cc
hirohata <43802663+hitohata@users.noreply.github.com>
2023-01-31 17:30:54 +0900

    update

--- src/bin/a03.rs
+++ src/bin/a03.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: [u32; 2],
+        p: [u32; n[0]],
+        q: [u32; n[0]],
+    }
+
+    for pp in &p {
+        for qq in &q {
+            if pp + qq == n[1] {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
--- src/bin/a04.rs
+++ src/bin/a04.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut n: u32
+    };
+    
+    const DIGIT: u32 = 10;
+
+    for i in (0..DIGIT).rev() {
+
+        let nn = n / 2_u32.pow(i);
+
+        print!("{}", nn % 2);
+    }
+    println!();
 }
