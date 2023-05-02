68b22ec2f9d304b8e82a701e24fd2b4348881fb1
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-02 17:17:28 +0900

    add a10

--- src/bin/a09.rs
+++ src/bin/a09.rs
@@ -1,3 +1,68 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        h: usize,
+        w: usize,
+        n: usize,
+        abcd: [[usize; 4]; n]
+    }
+
+    let mut answer = vec![vec![0_isize; w + 1]; h + 1];
+
+
+    for point in abcd.iter() {
+        marking(&mut answer, point.to_vec());
+    }
+
+    cumulative_sum(&mut answer, h, w);
+
+    print_answer(&answer, h, w);
+
+}
+
+fn cumulative_sum(grid: &mut Vec<Vec<isize>>, y_size: usize, x_size: usize) {
+
+    for y in 0..y_size {
+        for x in 0..x_size {
+            if x == 0 {
+                continue;
+            }
+            grid[y][x] += grid[y][x - 1]
+        }
+    }
+
+    for y in 0..y_size {
+        for x in 0..x_size {
+            if y == 0 {
+                continue;
+            }
+            grid[y][x] += grid[y - 1][x]
+        }
+    }
+
+}
+
+fn print_answer(grid: &Vec<Vec<isize>>, y_size: usize, x_size: usize) {
+    for y in 0..y_size {
+        for x in 0..x_size {
+            print!("{} ", grid[y][x]);
+            if x == x_size - 1 {
+                print!("\n");
+            }
+        }
+    }
+}
+
+fn marking(grid: &mut Vec<Vec<isize>>, point: Vec<usize>) {
+    let min_y = point[0] - 1;
+    let min_x = point[1] - 1;
+    let max_y = point[2] - 1;
+    let max_x = point[3] - 1;
+
+    grid[min_y][min_x] += 1;
+    grid[max_y + 1][max_x + 1] += 1;
+    grid[min_y][max_x + 1] -= 1;
+    grid[max_y + 1][min_x] -= 1;
 }
--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -1,3 +1,45 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [usize; n],
+        d: usize,
+        range: [[usize; 2]; d]
+    }
+
+    let mut cumulative_max_from_left: Vec<usize> = vec![0; n];
+    let mut cumulative_max_from_right: Vec<usize> = vec![0; n];
+
+    for i in 0..n {
+
+        if i == 0 {
+            cumulative_max_from_left[0] = a[0];
+            continue;
+        }
+
+        cumulative_max_from_left[i] = std::cmp::max(a[i], cumulative_max_from_left[i - 1]);
+    }
+
+
+    for i in (0..n).rev() {
+
+        if i == (n - 1) {
+            cumulative_max_from_right[i] = a[i];
+            continue;
+        }
+
+        cumulative_max_from_right[i] = std::cmp::max(a[i], cumulative_max_from_right[i + 1]);
+    }
+
+    for i in range.iter() {
+
+        let left = cumulative_max_from_left[0..i[0] - 1].iter().max().unwrap();
+        let right = cumulative_max_from_right[i[1]..n].iter().max().unwrap();
+
+        println!("{}", std::cmp::max(left, right))
+    }
+
+
 }
