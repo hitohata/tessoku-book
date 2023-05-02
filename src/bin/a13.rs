e5349036f81b8a1f246d602cb6b71934b541cb46
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-02 22:26:52 +0900

    add a13

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        k: usize,
+        mut a: [usize; n]
+    }
+
+    a.insert(0, 0);
+
+    let mut r = vec![0; n + 1];
+
+    for i in 1..n {
+        if i == 1 {
+            r[i] = 1;
+        } else {
+            r[i] = r[i - 1];
+        }
+
+        while (r[i] < n) && (a[r[i] + 1] - a[i] <= k) {
+            r[i] += 1;
+        }
+    }
+
+    let mut answer = 0_usize;
+
+    for i in 1..n {
+        answer += r[i] - i
+    }
+
+    println!("{:?}", answer);
+
 }
