ac674fc0f18ee5088e518abceebad2b0e5198bee
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-02 15:33:50 +0900

    a08

--- src/bin/a08.rs
+++ src/bin/a08.rs
@@ -4,43 +4,48 @@ fn main() {
     input! {
         h: usize,
         w: usize,
-        x: [[u32; w]; h],
+        x: [[usize; w]; h],
         q: usize,
         abcd: [[usize; 4]; q]
     }
 
 
-    let z: Vec<Vec<u32>> = two_dimension_sum(x, h, w);
+    let z: Vec<Vec<usize>> = two_dimension_sum(x, w, h);
 
     for i in abcd.iter() {
-        let [a, b, c, d] = [i[0] - 1, i[1] - 1, i[2] - 1, i[3] - 1];
-        let answer = z[c][d] + z[a - 1][b - 1] - z[a - 1][d] - z[c][b - 1];
+        let [a, b, c, d]: [usize; 4] = [i[0], i[1], i[2], i[3]];
+        let answer = z[c][d] + z[a - 1][b - 1] - z[c][b - 1] - z[a - 1][d];
         println!("{}", answer);
     }
 
 
 }
 
-fn two_dimension_sum(two_dimension: Vec<Vec<u32>>, size_x: usize, size_y: usize) -> Vec<Vec<u32>> {
-    let mut sum: Vec<Vec<u32>> = vec![vec![0; size_x]; size_y];
+fn two_dimension_sum(two_dimension: Vec<Vec<usize>>, size_x: usize, size_y: usize) -> Vec<Vec<usize>> {
+    let mut sum: Vec<Vec<usize>> = vec![vec![0; size_x + 1]; size_y + 1];
 
-    for y in 0..size_y {
-        for x in 0..size_x {
+    for y in 1..=size_y {
+        for x in 1..=size_x {
+
             if x == 0 {
-                sum[y][x] = two_dimension[y][x];
+                sum[y][x] = 0;
                 continue;
-            };
-            sum[y][x] = sum[y][x - 1] + two_dimension[y][x];
-        }
-    };
+            }
 
-    for x in 0..size_x {
-        for y in 0..size_y {
-            if y == 0 {
+            sum[y][x] = sum[y][x - 1] + two_dimension[y - 1][x - 1];
+        }
+    }
+
+    for y in 1..=size_y {
+        for x in 1..=size_x {
+
+            if x == 0 {
                 continue;
-            };
-            sum[y][x] = sum[y - 1][x] + sum[y][x];
-        };
+            }
+
+            sum[y][x] += sum[y - 1][x];
+
+        }
     }
 
     sum
