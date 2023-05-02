121ba146189fddcfc92757006fc2606c3a89372c
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-02 20:11:12 +0900

    add a11

--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        x: usize,
+        mut a: [usize; n]
+    }
+
+    a.insert(0, 0);
+
+    let mut min = 1;
+    let mut max = n;
+
+    while min <= max {
+        let center = (max + min) / 2;
+
+        if a[center] == x {
+            println!("{}", center);
+            break;
+        }
+
+        if x < a[center] {
+            max = center - 1;
+        }
+
+        if a[center] < x {
+            min = center + 1;
+        }
+
+    }
+
 }
